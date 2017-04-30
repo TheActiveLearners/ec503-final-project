0e79c4d539e7421806ae7e574ea53d8a9fbb48c5
JennaW <warren.jenna@gmail.com>
2017-04-30 11:26:36 -0400

    make example images

---
+++ example/plot_svm_hyp.m
@@ -0,0 +1,19 @@
+function [] = plot_svm_hyp(svmModel, X_data)
+
+bias = svmModel.Bias;
+supp_vecs = X_data(svmModel.IsSupportVector,:);
+
+% parameters from svmMdl
+% w1 = dot(svmModel.Alpha, supp_vecs(1,:));
+% w2 = dot(svmModel.Alpha, supp_vecs(2,:));
+
+w1 = svmModel.Beta(1);
+w2 = svmModel.Beta(2);
+
+% y = a*x + b
+a = -w1/w2;
+b = -bias/w2;
+X = -6:.1:8;
+Y = a*X + b;
+plot(X,Y);
+end
